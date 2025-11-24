802308aa1f0814d168643879b4d2528327f85231
Craig Omore <craiggomore@gmail.com>
2025-11-24 15:36:26 +0300

    labzz

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
+# Task 1: Return all even numbers in the input list
 def return_evens(num_list):
-    pass
+    # List comprehension to filter out even numbers
+    return [n for n in num_list if n % 2 == 0]
 
+# Task 2: Add an exclamation mark to each sentence in the input list
 def make_exclamation(sentence_list):
-    pass
+    # List comprehension to add "!" to each sentence
+    return [sentence + "!" for sentence in sentence_list]
